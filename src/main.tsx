4527017678d21e195922ffbe7774b0490eec3410
Lyonict <charles42.h@gmail.com>
2023-09-19 11:10:00 +0200

    Initial creation of router sytem

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,28 @@ import 'bootstrap/dist/css/bootstrap.css';
 // Store & redux
 import store from './app/store';
 import { Provider } from 'react-redux';
+// Router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/Root'
+import Home from './routes/Home'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      {
+        index: true,
+        element: <Home/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
 )
